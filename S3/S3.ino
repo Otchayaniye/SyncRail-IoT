0a212d0b51916fbe8f6b71b87622ed46aacb6772
Vinicius-11340 <vinicius_zakalusny@estudante.sesisenai.org.br>
2025-11-26 11:47:17 -0300

    Finalização do código

--- S3/S3.ino
+++ S3/S3.ino
@@ -19,6 +19,11 @@ void setup() {
   pinMode(ULTRA_TRIG3, OUTPUT);
   pinMode(Servo1, OUTPUT);
   pinMode(Servo2, OUTPUT);
+  Servo1.attach(Servo1);
+  Servo2.attach(Servo2);
+
+  Servo1.write(0);
+  Servo2.write(0);
 
   wificlient.setInsecure();
   Serial.begin(115200);    //configura a placa pra mostrar na tela
@@ -28,26 +33,25 @@ void setup() {
     Serial.print(".");
     delay(200);
   }
+
   Serial.println("Conectado com Sucesso parceiro!");
   Serial.println("Conectando no Broker");
   mqtt.setServer(BROKER_URL, BROKER_PORT);
   String boardID = "S3-";
   boardID += String(random(0xffff), HEX);
 
-  while (!mqtt.connect(boardID.c_str(),BROKER_USR_NAME,BROKER_USR_PASS)) {
+  while (!mqtt.connect(boardID.c_str(), BROKER_USR_NAME, BROKER_USR_PASS)) {
     Serial.print(".");
     delay(200);
   }
+
   Serial.println("\nConectado com sucesso ao broker!");
-  mqtt.subscribe(TOPIC_ILUMINACAO);         // RECEBER informações do tópico
+  mqtt.subscribe(TOPIC_Iluminacao);  // RECEBER informações do tópico
   mqtt.subscribe(TOPIC_Presenca1);
   mqtt.subscribe(TOPIC_Presenca2);
   mqtt.subscribe(TOPIC_Presenca3);
   mqtt.setCallback(callback);
-
-  
-  Servo1.write(0); 
-  Servo2.write(0);
+}
 
 long lerDistancia() {
   digitalWrite(ULTRA_TRIG3, LOW);
@@ -55,25 +59,24 @@ long lerDistancia() {
   digitalWrite(ULTRA_TRIG3, HIGH);
   delayMicroseconds(10);
   digitalWrite(ULTRA_TRIG3, LOW);
-  
+
   long duracao = pulseIn(ULTRA_ECHO3, HIGH);
   long distancia = duracao * 349.24 / 2 / 10000;
-  
   return distancia;
 }
 
 void sensor() {
   long distancia = lerDistancia();
-  
+
   Serial.print("Distância: ");
   Serial.print(distancia);
   Serial.println(" cm");
-  
+
   if (distancia < 10) {
     Serial.println("Objeto próximo!");
     mqtt.publish(TOPIC_Presenca3, "detectado");
   }
-  
+
   delay(500);
 }
 
@@ -86,42 +89,40 @@ void callback(char* topic, byte* payload, unsigned long length) {
   for (int i = 0; i < length; i++) {
     message += (char)payload[i];
   }
-  if (topic == TOPIC_ILUMINACAO){
-    if (message == "Acender"){
+
+  Serial.println(topic, ": ", message);
+
+  if (topic == TOPIC_Iluminacao) {
+    if (message == "Acender") {
       digitalWrite(Led, HIGH);
-    } else if (message == "Apagar"){
+
+    } else if (message == "Apagar") {
       digitalWrite(Led, LOW);
     }
-  }  
+  }
 
-  if (topic == TOPIC_Presenca1)
+  else if (topic == TOPIC_Presenca1) {
     if (message == "detectado")
+      for (int pos = 0; pos <= 90; pos += 1) {
+        Servo1.write(pos);
+        delay(15);
+      }
+  }
+
+  else if (topic == TOPIC_Presenca2) {
+    if (message == "detectado")
+      for (int pos = 0; pos <= 90; pos += 1) {
+        Servo2.write(pos);
+        delay(15);
+      }
+  }
+
+  else if (topic == TOPIC_Presenca3) {
+    if (message == "detectado")
+      for (int pos = 0; pos <= 120; pos += 1) {
+        Servo1.write(pos);
+        Servo2.write(pos);
+        delay(15);
+      }
+  }
 }
-
-
-
-
- - ler o ultrassonico - tem no class o código
- - ver se o valor é < 10 -
-	- publica (presenca3) - "detectado" mqtt.publish(TOPIC_PRESENCA_3, "detectado")
-
-call-back(topic, payload , length)
-
- - se topic == iluminação
-	- se mensagem == "Acender"
-		- acende led
-	- senão se mensagem == "Apagar"
-		- apaga led
-
- - senão se topic == presenca1
-	- se mensagem == "detectado"
-		- servo1 para 90º
-
- - senão se topic == presenca2
-	- se mensagem == "detectado"
-		- servo2 para 90º
-
- - senão se topic == presenca3
-	- se mensagem == "detectado"
-		- servo1 para 120º
-		- servo2 para 120º
